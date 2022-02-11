9dfec593d83fb3ff8e472addbb81eeb9fd4224a2
Jay <webmarket2k@gmail.com>
2022-02-11 20:21:18 +0800

    adding tailwind ui

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <div>App Development</div>
+        </div>
+    );
+};
+
+
+export default App;
